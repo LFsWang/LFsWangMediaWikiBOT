ac861a6173636bfcb92765e19f0f676ac066cb37
LFsWang <tnst92002@gmail.com>
2014-09-06 04:36:03 +0000

    add gitignore

--- lib/setting.example.php
+++ lib/setting.example.php
@@ -1,1 +1,7 @@
-<??>
+<?php
+$settings['wikiroot'] = 'http://en.wikipedia.org';
+$settings['wikiapi'] = $settings['wikiroot'].'/w/api.php';
+$settings['user'] = 'username';
+$settings['pass'] = 'password';
+$settings['cookiefile'] = 'cookies.tmp';
+?>
